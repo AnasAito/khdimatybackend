1fee0995b2a8c9690dbf25eae000f0949a82803f
khdimaty <anasaitaomar1999@gmail.com>
2019-10-08 15:42:48 +0100

    comm

--- src/index.js
+++ src/index.js
@@ -1,43 +1,44 @@
-const { GraphQLServer } = require('graphql-yoga')
-const { prisma } = require('./generated/prisma-client')
+const { GraphQLServer } = require("graphql-yoga");
+const { prisma } = require("./generated/prisma-client");
 
 const resolvers = {
   Query: {
     feed: (parent, args, context) => {
-      return context.prisma.posts({ where: { published: true } })
+      //console
+      return context.prisma.posts({ where: { published: true } });
     },
     drafts: (parent, args, context) => {
-      return context.prisma.posts({ where: { published: false } })
+      return context.prisma.posts({ where: { published: false } });
     },
     post: (parent, { id }, context) => {
-      return context.prisma.post({ id })
-    },
+      return context.prisma.post({ id });
+    }
   },
   Mutation: {
     createDraft(parent, { title, content }, context) {
       return context.prisma.createPost({
         title,
-        content,
-      })
+        content
+      });
     },
     deletePost(parent, { id }, context) {
-      return context.prisma.deletePost({ id })
+      return context.prisma.deletePost({ id });
     },
     publish(parent, { id }, context) {
       return context.prisma.updatePost({
         where: { id },
-        data: { published: true },
-      })
-    },
-  },
-}
+        data: { published: true }
+      });
+    }
+  }
+};
 
 const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
+  typeDefs: "./src/schema.graphql",
   resolvers,
   context: {
-    prisma,
-  },
-})
+    prisma
+  }
+});
 
-server.start(() => console.log('Server is running on http://localhost:4000'))
+server.start(() => console.log("Server is running on http://localhost:4000"));
